f3eaca00ac286d99b8d725e63dce12f58e1ec711
Chung Pham <chung14799@gmail.com>
2024-10-15 00:33:11 +0700

    fix px

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -2,10 +2,10 @@ import { LogoFooter } from "../../assets";
 
 const Footer = () => {
   return (
-    <footer className="bg-white h-[40px] w-full border-t-[#a1a1a1] border-t">
+    <footer className="bg-white h-[4.3vh] w-full border-t-[#a1a1a1] border-t">
       <div className="w-full h-full flex items-center justify-center gap-2">
-        <img src={LogoFooter} alt="logo-footer" className="w-[20px] h-[24px]" />
-        <div className="text-gray-800 text-[11px]">
+        <img src={LogoFooter} alt="logo-footer" className="w-[5vw]" />
+        <div className="text-[#333333] text-[2.6vw]">
           90/88B Nguyen Dinh Chieu, Da Kao Ward, District 1, Ho Chi Minh City
         </div>
       </div>
--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,9 +3,9 @@ import { EmailIcon } from "../../assets/icons/Email";
 
 const Header = () => {
   return (
-    <div className="h-[40px] w-full bg-white flex items-center justify-between pl-2">
-      <img src={Logo} alt="logo" className="w-[100px] h-auto" />
-      <div className="w-[40px] h-full bg-[#F98F29] flex items-center justify-center">
+    <div className="h-[4.3vh] w-full bg-white flex items-center justify-between pl-2">
+      <img src={Logo} alt="logo" className="w-[25vw] h-auto" />
+      <div className="w-[10vw] h-full bg-[#F98F29] flex items-center justify-center">
         <EmailIcon />
       </div>
     </div>
